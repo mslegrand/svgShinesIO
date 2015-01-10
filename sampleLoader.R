425192fadfe5ce89c8927829b6ace5b18a228163
sup <legrand@coanalytics.net>
2015-01-10 16:43:44 -0500

    cleaned code

--- sampleLoader.R
+++ sampleLoader.R
@@ -1,12 +1,3 @@
-# for(i in 1:38){
-#   fileName=paste0("samples/",categories[i],"/",tmp[i],".R")
-#   content<-paste(grps[[i]], collapse="\n")
-#   content<-gsub("},$", "}\n", content)
-#   sink(fileName)
-#   cat(content )
-#   sink()
-# }
-
 
 #helper fn
 extractField1<-function(fieldName,grp){
@@ -18,22 +9,6 @@ extractField1<-function(fieldName,grp){
     str_trim(gsub(pattern, "", grp[indx])  )
   }
 }
-
-#to get a list of all categories
-#either get from sample.index
-# do a dir
-
-
-#read categories
-catChoice<-dir("samples")
-
-#to load a samples list we would
-# scan the director and load all 
-#read and parse each file
-
-"Animations"->category
-# readLines(fileName)->lines
-# extractField1(lines,"Title")->title
 
 #create list of titles and fileNames for category
 readCatFiles<-function(category){
@@ -49,20 +24,6 @@ readCatFiles<-function(category){
   as.list(cf)
 }
 
-# for each fileName, 
-#  1. readfile
-#  2. extract title
-#  3. title is fed to choices of radio buttons
-
-# fileName is recorded for loading file
-
-#!!!need list with title, fileName
-
-# upon request by server, load file
-# fill description 
-# fill (title) name, concepts in ui
-# extract and run code.
-
 getFileInfo<-function(fileName){
   #read the file
   readLines(fileName)->lines 
@@ -74,7 +35,7 @@ getFileInfo<-function(fileName){
     description=extractField1("description",lines),
     concepts=extractField1("concepts",lines),
     src=paste(lines,collapse="\n")
-  )
+  )    
 }
 
 
